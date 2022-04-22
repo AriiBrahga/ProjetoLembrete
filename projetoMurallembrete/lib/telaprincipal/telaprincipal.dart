edd2b2870cbde8bf1dcd804ed069e8c7d8a5b463
Arielson da Silva Braga <ariel.braga09@gmail.com>
2022-04-22 10:36:19 -0300

    mudei

--- projetoMurallembrete/lib/telaprincipal/telaprincipal.dart
+++ projetoMurallembrete/lib/telaprincipal/telaprincipal.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_import
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
